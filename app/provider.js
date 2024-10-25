87a6e2acab4727ff80bae7c40aa77815c0f08cc3
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-25 10:41:40 -0700

    adding context to making userdetails available

--- app/provider.js
+++ app/provider.js
@@ -1,10 +1,13 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { UserDetailContext } from "./_context/UserDetailContext";
 
 function Provider({ children }) {
   const { user } = useUser();
+  const [userDetail, setUserDetail] = useState([]);
+
   useEffect(() => {
     user && VerifyUser();
   }, [user]);
@@ -12,10 +15,14 @@ function Provider({ children }) {
   // verify user
   const VerifyUser = async () => {
     const dataResult = await axios.post("/api/verify-user", { user: user });
-    console.log(dataResult.data);
+    setUserDetail(dataResult.data.result);
   };
 
-  return <div>{children}</div>;
+  return (
+    <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+      <div>{children}</div>
+    </UserDetailContext.Provider>
+  );
 }
 
 export default Provider;
