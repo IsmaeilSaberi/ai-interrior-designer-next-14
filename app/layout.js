5cd16982a5f941e32af2ef6e5c9083f314278b2f
Ismaeil <ismaeilsaberi@gmail.com>
2024-10-25 10:35:28 -0700

    adding protection to dashboard page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Outfit } from "next/font/google";
+import Provider from "./provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={outfit.className}>{children}</body>
+        <body className={outfit.className}>
+          <Provider>{children}</Provider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./config/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "",
+    url: "postgresql://content%20generator%20saas_owner:3aeV1tzpIgDs@ep-orange-sea-a2qqwsws.eu-central-1.aws.neon.tech/ai-interrior-designer?sslmode=require",
   },
 });
