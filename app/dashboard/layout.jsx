3875a36eacf8c15d673ce7130f9742dfe419c6ed
Ismaeil <ismaeilsaberi@gmail.com>
2024-11-08 01:04:53 -0800

    adding other components

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -5,7 +5,7 @@ function DashboardLayout({ children }) {
   return (
     <div>
       <Header />
-      <div className="pt-20 px-10 md:px-20 lg:px-40 xl:px-60">{children}</div>
+      <div className="pt-16 px-10 md:px-20 lg:px-40 xl:px-60">{children}</div>
     </div>
   );
 }
